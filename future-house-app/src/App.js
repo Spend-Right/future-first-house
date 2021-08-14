5877722e9124d9e38c5444badb9a19bca7f62538
Tian Welgemoed <tian.welgemoed@gmail.com>
2021-08-14 15:36:24 +1200

    set up navigation

--- future-house-app/src/App.js
+++ future-house-app/src/App.js
@@ -1,14 +1,37 @@
+import { useState } from 'react';
+import {
+    Route,
+    NavLink,
+    HashRouter
+} from "react-router-dom";
+import Home from "./pages/Index";
+import StatusChecker from "./pages/StatusChecker";
+import HouseGoal from "./pages/HouseGoal";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Future house app</h1>
-        <p></p>
-      </header>
-    </div>
-  );
+    
+    return (
+        <div className="App">
+            <HashRouter>
+                <div>
+                    <h1>Future House</h1>
+
+                    <ul className="header">
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="/statuschecker">Status Checker</NavLink></li>
+                        <li><NavLink to="/housegoal">House Goal</NavLink></li>
+                    </ul>
+
+                    <div className="content">
+                        <Route path="/" component={Home}/>
+                        <Route path="/statuschecker" component={StatusChecker}/>
+                        <Route path="/housegoal" component={HouseGoal}/>
+                    </div>
+                </div>
+            </HashRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ future-house-app/src/pages/HouseGoal.js
@@ -0,0 +1,28 @@
+
+function HouseGoal() {
+    return (
+        <div>
+            <header>
+                <h1>House Goal </h1>
+            </header>
+            <body>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                    molestiae quas vel sint commodi repudiandae consequuntur. 
+                </p>
+                <form>
+                    <label for="fname">Income after tax:</label><br/>
+                    <input type="text" id="fname" value="Mike"></input><br/><br/>
+
+                    <label for="lname">Average Expenses</label><br/>
+                    <input type="text" id="lname" value="Walker"></input><br/><br/>
+                    
+                    <input type="submit" value="Submit"></input>
+                </form>
+            </body>
+        </div>
+    );
+}
+  
+  export default HouseGoal;
+  
